40202787009896a9701dbe091face869fd688f37
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-05-09 13:16:23 +0000

    Add base tailwind styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
+   
+  validates :username, length: { maximum: 15 }
+
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+_{};':"\\|,.<>?]).{8,30}\z/,
+    message: "must include at least one letter, one digit and one special character and be between 8 and 30 characters long" }
 end
