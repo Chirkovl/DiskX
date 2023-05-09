4447a53e4128611012b9d7c77a98158c2ef7cda7
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-05-09 19:32:47 +0400

    Add username in devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
    
-  validates :username, length: { maximum: 15 }
+  # validates :username, length: { maximum: 15 }
 
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+_{};':"\\|,.<>?]).{8,30}\z/,
-    message: "must include at least one letter, one digit and one special character and be between 8 and 30 characters long" }
+  # validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+_{};':"\\|,.<>?]).{8,30}\z/,
+  #   message: "must include at least one letter, one digit and one special character and be between 8 and 30 characters long" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_160607) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_131713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_160607) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
